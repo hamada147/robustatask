d6493726222e4fe844868ae598bfcb7d6c45485e
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-12-06 12:38:29 +0200

    Test calling obj-c from swift

--- robustatask/ViewController.swift
+++ robustatask/ViewController.swift
@@ -13,7 +13,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let connector = RepositoriesConnector(delegate: self)
+        connector?.getRepositories(0)
+    }
 
 
 }
 
+extension ViewController: ConnectorDelegate {
+    func callCompleted(_ response: Any!) {
+        if (response is RepositoriesResponse) {
+            let actualResponse = response as! RepositoriesResponse
+            let repos = actualResponse.repositories as? [Repository]
+            
+            print(repos?.count ?? 0)
+            
+        } else {
+            // TODO: Show error here
+        }
+    }
+    
+    func callFailed(_ response: ErrorResponse!) {
+        // TODO: Show error here
+    }
+}
