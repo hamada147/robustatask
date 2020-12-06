985fe569c22be91c492084b97140025bd9678168
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-12-06 12:27:52 +0200

    Constants

---
+++ robustatask/NetworkLib/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  testnetworkinObjc
+//
+//  Created by Moussa on 03/12/2020.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+NSString* baseURL = @"https://api.github.com/";
+
+
+#endif /* Constants_h */
