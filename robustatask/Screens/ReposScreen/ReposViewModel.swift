cd6bfeb9852b84c575726d838be8200b216a3246
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-12-06 16:56:28 +0200

    ReposViewModel

---
+++ robustatask/Screens/ReposScreen/ReposViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ReposViewModel.swift
+//  robustatask
+//
+//  Created by Moussa on 06/12/2020.
+//
+
+import Foundation
+
+protocol ReposViewModelDelegate {
+    func didGetRepositories(_ repos: [RepositoryModel])
+    func errorInRetrivingData(error: ErrorResponse)
+}
+
+class ReposViewModel {
+    lazy var reposity: RepositoriesRepository = RepositoriesRepository(delegate: self)
+    var delegate: ReposViewModelDelegate? = nil
+    
+    func getRepositories(since: Int) {
+        self.reposity.getRepositories(since: since)
+    }
+}
+
+extension ReposViewModel: RepositoryDelegate {
+    func onSuccess(response: Any) {
+        if (response is [RepositoryModel]) {
+            let repos = response as! [RepositoryModel]
+            self.delegate?.didGetRepositories(repos)
+        } else {
+            LoggingManager.logError("Missed handling in ReposViewModel-onSuccess")
+        }
+    }
+    
+    func onFailed(error: ErrorResponse) {
+        self.delegate?.errorInRetrivingData(error: error)
+    }
+}
