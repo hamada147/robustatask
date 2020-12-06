c003c7ff6e2bf6d0b3526de86d809203a4df8d3e
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-12-06 12:27:29 +0200

    ErrorResponse

---
+++ robustatask/NetworkLib/ErrorResponse.h
@@ -0,0 +1,18 @@
+//
+//  ErrorResponse.h
+//  testnetworkinObjc
+//
+//  Created by Moussa on 03/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef ErrorResponse_h
+#define ErrorResponse_h
+
+@interface ErrorResponse : NSObject
+@property (nonatomic) BOOL* isSuccess;
+@property (strong, nonatomic) NSString* errorMessage;
+@end
+
+#endif /* ErrorResponse_h */
---
+++ robustatask/NetworkLib/ErrorResponse.m
@@ -0,0 +1,12 @@
+//
+//  ErrorResponse.m
+//  testnetworkinObjc
+//
+//  Created by Moussa on 03/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+#import "ErrorResponse.h"
+
+@implementation ErrorResponse
+@end
